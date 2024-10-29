4aeca6dfb10eeb92f720856209c3ee3cdcde8400
Jeremy Schur <jeremy.schur02@gmail.com>
2024-10-28 21:38:14 -0600

    Upload

---
+++ backend/controllers/auth/refreshTokenController.js
@@ -0,0 +1,24 @@
+const User = require('../../model/User');
+const jwt = require('jsonwebtoken');
+
+const handleRefreshToken = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) {
+        return res.sendStatus(401);
+    }
+    const refreshToken = cookies.jwt;
+    const user = await User.findOne({ refreshToken }).exec();
+    if (!user) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+        if (err || user.username !== decoded.username) {
+            return res.sendStatus(403);
+        }
+        const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+        return res.status(200).json({ accessToken });
+    });
+};
+
+module.exports = { handleRefreshToken };
